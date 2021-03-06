1158faa76cee76dae8f5d1a047ce3533a602ded3
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-06 18:22:47 +0000

    Complete all bulletpoints.

--- DatingProfile/DatingProfile/Profile.cs
+++ DatingProfile/DatingProfile/Profile.cs
@@ -6,5 +6,49 @@ namespace DatingProfile
 {
     class Profile
     {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(
+            string name,
+            int age,
+            string city = "n/a",
+            string country = "n/a",
+            string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string bio = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}\n";
+
+            if(this.hobbies.Length > 0)
+            {
+                bio += "\nHobbies:\n";
+                foreach (string hobby in hobbies)
+                {
+                    bio += $"- {hobby}\n";
+                }
+            }
+
+            
+
+            return bio;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
     }
 }
--- DatingProfile/DatingProfile/Program.cs
+++ DatingProfile/DatingProfile/Program.cs
@@ -6,7 +6,10 @@ namespace DatingProfile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Profile sam = new Profile("Sam Drakkila", 30);
+            sam.SetHobbies(new string[] {"listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns" });
+            Console.WriteLine(sam.ViewProfile());
+
         }
     }
 }
