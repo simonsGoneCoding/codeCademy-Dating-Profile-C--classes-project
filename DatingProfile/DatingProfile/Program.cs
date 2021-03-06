41b1e12d12d4cd0191a09e70ac2850b0c3944e24
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-06 17:16:41 +0000

    Initial commit - codeCademy - classes project.

---
+++ DatingProfile/DatingProfile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
